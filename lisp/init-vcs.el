dc22710b9a8c98547746afbff1cebbb3ae6ea681
curtain <keleidoscope@163.com>
2022-03-10 15:46:25 +0800

    make magit buffer fullscreen

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -2,6 +2,7 @@
 
 (use-package magit
   :config
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   (with-eval-after-load 'evil
     (evil-add-hjkl-bindings magit-status-mode-map
       'emacs

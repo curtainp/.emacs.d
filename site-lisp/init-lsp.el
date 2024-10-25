a0c1f8578b531054f7fc9136c5ed1e50f3a9893d
curtainp <keleidoscope@163.com>
2024-10-25 14:14:10 +0800

    update lsp

--- site-lisp/init-lsp.el
+++ site-lisp/init-lsp.el
@@ -1,4 +1,10 @@
-;; -*- lexical-binding: t -*- 
+;; -*- lexical-binding: t -*-
+
+(defun get-tailwindcss-language-id-in-react (project-path file-path server-name extension-name)
+  (when (string-equal server-name "tailwindcss")
+    (if (string-equal extension-name "tsx")
+	"typescriptreact"
+      "")))
 
 (use-package lsp-bridge
   :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
@@ -14,8 +20,17 @@
   (acm-enable-codeium nil)
   (acm-enable-lsp-workspace-symbol t)
   (lsp-bridge-enable-inlay-hint t)
+  (lsp-bridge-get-language-id 'get-tailwindcss-language-id-in-react)
   (lsp-bridge-user-langserver-dir (concat (expand-file-name user-emacs-directory) "langserver"))
   (lsp-bridge-user-multiserver-dir (concat (expand-file-name user-emacs-directory) "multiserver"))
+  (lsp-bridge-enable-log t)
+  (lsp-bridge-multi-lang-server-extension-list
+	'(
+	  (("ts")   . "typescript_eslint")
+	  (("tsx")  . "typescriptreact_tailwindcss")
+	  (("jsx")  . "javascriptreact_tailwindcss")
+	  (("html") . "html_tailwindcss")
+	  (("css")  . "css_tailwindcss")))
   :bind (:map global-map
 	      ("M-." . lsp-bridge-find-def)
 	      ("M-," . lsp-bridge-find-def-return)
@@ -27,15 +42,6 @@
 	      ("C-c C-n" . lsp-bridge-rename)
 	      ("C-c C-f" . lsp-bridge-code-format))
   :init
-  (global-lsp-bridge-mode)
-  :config
-  (setq lsp-bridge-multi-lang-server-extension-list
-	'(
-	  (("ts")   . "typescript_eslint")
-	  (("tsx")  . "typescriptreact_eslint_tailwindcss")
-	  (("jsx")  . "javascriptreact_eslint_tailwindcss")
-	  (("html") . "html_tailwindcss")
-	  (("css")  . "css_tailwindcss")))
-  )
+  (global-lsp-bridge-mode))
 
 (provide 'init-lsp)

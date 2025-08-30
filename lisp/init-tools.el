65881aae642c67af0b09cc850b38cd3ac516bd77
curtainp <curtainwk@gmail.com>
2025-08-31 00:17:03 +0800

    🔧 enable emoji display with commit buffer

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -14,6 +14,7 @@
   :straight (:host github :repo "Spike-Leung/gitmoji")
   :config
   (setq gitmoji-selection-backend '(consult))
+  (setq gitmoji--insert-utf8-emoji t)
   (setq gitmoji--display-utf8-emoji t))
 
 (use-package list-unicode-display

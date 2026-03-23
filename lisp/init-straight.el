3434f4752cf59c03b9595e59de5786c2d8a59f01
curtainp <curtainwk@proton.me>
2026-03-23 20:26:45 +0800

    clean unused files

--- lisp/init-straight.el
+++
@@ -1,7 +0,0 @@
-;;; -*- lexical-binding: t -*-
-(setq use-package-always-demand (daemonp)
-      ;; use-package-always-defer (not (daemonp))
-      use-package-expand-minimally t
-      use-package-enable-imenu-support t)
-
-(provide 'init-straight)
